6e83d6ac6a07b125944f629cfa10ec6b4a05dd05
Sally Northmore <snorthmore@brilliantbasics.com>
2019-10-23 21:02:36 +0100

    Add helmet

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import useSiteMetadata from './SiteMetadata';
 // import { withPrefix } from 'gatsby';
+import { Helmet } from 'react-helmet';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, GlobalTheme } from '../StyleGuide/global';
 import Banner from '../Banner';
@@ -9,15 +10,23 @@ import Banner from '../Banner';
 import * as S from './styles';
 
 const Layout = ({ children }) => {
-  // const { title, description } = useSiteMetadata();
-
-  console.log(GlobalTheme);
-
   return (
     <>
       <ThemeProvider theme={GlobalTheme}>
         <S.Layout>
           <GlobalStyle />
+          <Helmet>
+            <html lang="en" />
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <meta property="og:type" content="business.business" />
+            <meta property="og:title" content={title} />
+            <meta property="og:url" content="/" />
+            <meta
+              property="og:image"
+              content={`${withPrefix('/')}img/og-image.jpg`}
+            />
+          </Helmet>
           <Banner />
           <div>{children}</div>
         </S.Layout>
