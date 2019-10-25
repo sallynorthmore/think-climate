492c1c42a6d440fc862b7b45fa2a499f7289383f
Sally Northmore <sally.northmore@ibm.com>
2019-10-25 16:44:17 +0100

    Style event aside

--- src/templates/event-post.styles.js
+++ src/templates/event-post.styles.js
@@ -28,8 +28,16 @@ export const Content = styled.div`
 export const Aside = styled.aside`
   text-align: left;
 
+  & p {
+    margin-top: 1rem !important;
+  }
+
   @media (min-width: ${props => props.theme.tablet}) {
     flex: 0 0 40%;
     margin-left: 4rem;
+
+    & p {
+      margin-top: 1rem !important;
+    }
   }
 `;
