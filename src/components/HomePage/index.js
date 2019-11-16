828ee39670db3e9956baf4ac745bbd66cdc2584d
Sally Northmore <sally.northmore@ibm.com>
2019-11-16 15:20:17 +0000

    Output profiles on homepage

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -2,31 +2,54 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Layout from '../Layout/index.js';
 import ParralaxHero from '../ParralaxHero';
+import PreviewCompatibleImage from '../PreviewCompatibleImage';
 import SEO from '../SEO';
 import EventRoll from '../EventRoll';
 import * as S from './styles';
 
-const HomePage = ({ title, hero }) => (
-  <S.HomePage>
-    <Layout>
-      <SEO
-        lang="en"
-        title="THINK Climate"
-        description="We can build a business, change the world and have fun."
-      />
-      <ParralaxHero
-        headline={hero.title}
-        smallText={hero.smalltext}
-        image={hero.image}
-      ></ParralaxHero>
-      <S.Main role="main">
-        <EventRoll />
-        {/* <Link to="/blog">Read more</Link> */}
-        {/* <Link to="/contact">Contact us</Link> */}
-      </S.Main>
-    </Layout>
-  </S.HomePage>
-);
+const HomePage = ({ hero, profiles }) => {
+  console.log(typeof profiles);
+  return (
+    <S.HomePage>
+      <Layout>
+        <SEO
+          lang="en"
+          title="THINK Climate"
+          description="We can build a business, change the world and have fun."
+        />
+        <ParralaxHero
+          headline={hero.title}
+          smallText={hero.smalltext}
+          image={hero.image}
+        ></ParralaxHero>
+        <S.Main role="main">
+          <EventRoll />
+          {/* <Link to="/blog">Read more</Link> */}
+          {/* <Link to="/contact">Contact us</Link> */}
+        </S.Main>
+        <div id="team">
+          <h2>Team</h2>
+          <ul>
+            {profiles &&
+              profiles.map(profile => (
+                <li>
+                  {profile.image && (
+                    <PreviewCompatibleImage
+                      className="eventImage"
+                      imageInfo={{
+                        image: profile.image,
+                        alt: `Image of ${profile.name}`
+                      }}
+                    />
+                  )}
+                </li>
+              ))}
+          </ul>
+        </div>
+      </Layout>
+    </S.HomePage>
+  );
+};
 
 HomePage.propTypes = {
   title: PropTypes.string,
@@ -35,7 +58,18 @@ HomePage.propTypes = {
     title: PropTypes.string,
     smallText: PropTypes.string,
     image: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
-  })
+  }),
+  profiles: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      description: PropTypes.string,
+      book: PropTypes.string,
+      bookLink: PropTypes.string,
+      film: PropTypes.string,
+      filmLink: PropTypes.string,
+      pledge: PropTypes.string
+    })
+  )
 };
 
 export default HomePage;
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,9 +4,11 @@ import { graphql } from 'gatsby';
 import HomePage from '../components/HomePage';
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
+  // const { frontmatter } = data.markdownRemark;
+  const page = data.page.frontmatter;
+  const profiles = data.profiles.frontmatter.profiles;
 
-  return <HomePage hero={frontmatter.hero} title={frontmatter.title} />;
+  return <HomePage hero={page.hero} profiles={profiles} />;
 };
 
 IndexPage.propTypes = {
@@ -20,8 +22,8 @@ IndexPage.propTypes = {
 export default IndexPage;
 
 export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+  query {
+    page: markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
 
@@ -32,6 +34,28 @@ export const pageQuery = graphql`
           image {
             childImageSharp {
               fluid(maxWidth: 2048, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+    profiles: markdownRemark(
+      frontmatter: { templateKey: { eq: "about-page" } }
+    ) {
+      frontmatter {
+        profiles {
+          name
+          description
+          pledge
+          bookLink
+          book
+          film
+          filmLink
+          image {
+            childImageSharp {
+              fluid(maxWidth: 500, quality: 80) {
                 ...GatsbyImageSharpFluid
               }
             }
