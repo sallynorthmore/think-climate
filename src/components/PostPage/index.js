1597d4c780836105f26ee6db5607aebf65fded80
Sally Northmore <sally.northmore@ibm.com>
2019-11-20 12:44:55 -0500

    Add seo to all pages

--- src/components/PostPage/index.js
+++ src/components/PostPage/index.js
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types';
 import Layout from '../Layout/index.js';
 import Hero from '../Hero';
 import Content from '../Content';
+import SEO from '../SEO';
 import * as S from './styles';
 
 const PostPage = ({ children, title, smallText }) => (
   <S.PostPage>
+    <SEO description={title} />
     <Layout>
       <Hero isSmall headline={title}></Hero>
       <S.Main>
